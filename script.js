c2292a13ef997ca0e00eb36ff3cfad320a468e9d
Carlo Untalan <152079967+carlo121603@users.noreply.github.com>
2025-01-16 22:06:54 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+const burgerMenu = document.querySelector('.burger-menu');
+const navLinks = document.querySelector('.nav-links');
+
+burgerMenu.addEventListener('click', () => {
+  navLinks.classList.toggle('active');
+});
+
+document.querySelectorAll('header .nav-links a, .footer-links a').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault(); // Prevent default anchor behavior
+
+    const targetId = this.getAttribute('href').substring(1); // Get the ID without the #
+    const targetElement = document.getElementById(targetId);
+
+    if (targetElement) {
+      const headerHeight = document.querySelector('header').offsetHeight; // Get header height
+      const targetPosition = targetElement.offsetTop - headerHeight; // Offset scroll position
+
+      window.scrollTo({
+        top: targetPosition,
+        behavior: 'smooth' // Smooth scrolling effect
+      });
+    }
+  });
+});
